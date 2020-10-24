2771532d465c462cf9a47d05af08defcdd9a91a7
Omer Siddiqui <oasiddiqui1@gmail.com>
2020-10-24 07:45:56 -0600

    Updates

---
+++ precache-manifest.9d3596c07ef966e5f607940e574e6d7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2d52dcbe20b8817c179235f47f0901",
+    "url": "/omerasiddiqui/fullstack-tech-challenge/index.html"
+  },
+  {
+    "revision": "46f78d0aafe72ddee8cd",
+    "url": "/omerasiddiqui/fullstack-tech-challenge/static/css/main.2b7ed6be.chunk.css"
+  },
+  {
+    "revision": "88f8b4cbac294bf52e78",
+    "url": "/omerasiddiqui/fullstack-tech-challenge/static/js/2.c05a9a4c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/omerasiddiqui/fullstack-tech-challenge/static/js/2.c05a9a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f78d0aafe72ddee8cd",
+    "url": "/omerasiddiqui/fullstack-tech-challenge/static/js/main.28facf43.chunk.js"
+  },
+  {
+    "revision": "d6a4da0480fb0caf1484",
+    "url": "/omerasiddiqui/fullstack-tech-challenge/static/js/runtime-main.efb6b54f.js"
+  }
+]);
